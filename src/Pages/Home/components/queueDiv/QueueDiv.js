1058ceadf5b8fb48e48b794df7e79a7e7d1a0717
Limon <limon9743@gmail.com>
2022-02-27 12:03:03 +0600

    12th_Update

--- src/Pages/Home/components/queueDiv/QueueDiv.js
+++ src/Pages/Home/components/queueDiv/QueueDiv.js
@@ -8,36 +8,8 @@ import MoneyOffIcon from "@material-ui/icons/MoneyOff";
 const queueDiv = (props) => {
   const { title, buttonName, cardIcon, buttonLink } = props;
   return (
-    <div className="divcontainer">
-      <div className="queue-div-heading-container">
-        <h3 className="queue-div-heading">Get A Quick Response</h3>
-      </div>
-      {/* <div className="buttonContainer">
-        <div className="buttonContainerSingle">
-          <span>Get Call First</span>
-          <Link to="/buyer-request">
-            <button className="custom-btn btn-5">
-              <span>Buyer</span>
-            </button>
-          </Link>
-        </div>
-        <div className="buttonContainerSingle">
-          <span>Get Email Alerts</span>
-          <Link to="/landlord-renter">
-            <button className="custom-btn btn-5">
-              <span>Renter Landlord</span>
-            </button>
-          </Link>
-        </div>
-        <div className="buttonContainerSingle">
-          <span>Get Price Reductions</span>
-          <Link to="/landlord-seller">
-            <button className="custom-btn btn-5">
-              <span>Seller Landlord</span>
-            </button>
-          </Link>
-        </div>
-      </div> */}
+    <div className="quick-response-div">
+      <h3>Get A Quick Response</h3>
       <div className="quick-response-fancy-card-div">
         <div className="quick-response-fancy-card-each">
           <QuickResponseCard
